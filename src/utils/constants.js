11328b8e5e58f951651fd905d776258346a190b5
Deepak <aryanrajj1234@gmail.com>
2026-04-12 17:55:55 +0530

    update code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export  const BASE_URL = "https://devtinder-backend-byvt.onrender.com"
+export  const BASE_URL = "https://dev-tinder-backend-ashy.vercel.app"
 // export  const BASE_URL = "http://localhost:3000"
